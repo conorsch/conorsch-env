c817409226981e36f3d7328c6cf822caeb9eca59
Conor Schaefer <conor.schaefer@gmail.com>
2019-11-11 22:46:43 -0800

    Updates Pipfile with python3 hardcoded

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,28 +20,26 @@ function is_likely_qubes() {
 }
 
 function configure_venv() {
-    #tmp_venv="$(mktemp -d)"
-    tmp_venv="${script_dir}/venv"
+    tmp_venv="$(mktemp -d)"
+    #tmp_venv="${script_dir}/venv"
     system_packages=""
     # Virtualenvs in Qubes have trouble discovering python-apt modules;
     if is_likely_qubes ; then
         system_packages="--system-site-packages"
     fi
-    if [[ ! -d "$tmp_venv" ]] ; then
-        virtualenv "$tmp_venv" "${system_packages}" --quiet
-    fi
+    virtualenv --python python3 "$tmp_venv" "${system_packages}"
     source "${tmp_venv}/bin/activate"
 }
 
 function run_ansible() {
     mkdir -p "${script_dir}/.roles"
-    pipenv run ansible-galaxy install -r "${script_dir}/requirements.yml" > /dev/null 2>&1
-    pipenv run ansible-playbook "${script_dir}/examples/localhost.yml"
+    ansible-galaxy install -r "${script_dir}/requirements.yml"
+    ansible-playbook "${script_dir}/examples/localhost.yml"
 }
 
 printf "Checking if pip is installed... "
-if ! is_installed pip ; then
-    sudo apt install -y python-pip
+if ! is_installed pip3 ; then
+    sudo apt install -y python3-pip
     printf "CHANGED.\n"
 else
     printf "OK.\n"
@@ -50,7 +48,7 @@ fi
 printf "Checking if pipenv is installed... "
 export PATH=$PATH:$HOME/.local/bin
 if ! is_installed pipenv ; then
-    pip install --user pipenv
+    pip3 install --user pipenv
     printf "CHANGED.\n"
 else
     printf "OK.\n"
@@ -61,11 +59,12 @@ printf "Checking if ansible is installed... "
 configure_venv
 if ! is_installed ansible > /dev/null 2>&1 ; then
     printf "no, installing... "
+    sudo apt-get install -y python3-apt -qq
     configure_venv
-    pipenv install > /dev/null 2>&1
+    pipenv --three install
     printf "CHANGED.\n"
-    run_ansible
 else
     printf "OK.\n"
 fi
 
+run_ansible
